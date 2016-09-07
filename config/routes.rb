175a7b8947d682addaa6767a5b9f435c6288d6ea
neerajmaurya07 <neerajbittern104@gmail.com>
2016-09-07 23:59:24 +0530

    article done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Blogger::Application.routes.draw do
   resources :articles
-  get "articles/new"
+  root 'articles#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
